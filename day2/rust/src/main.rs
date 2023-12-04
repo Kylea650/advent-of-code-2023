ba61ee46173df49e963cfbc9d350575f553711ef
Kylea650 <90274941+Kylea650@users.noreply.github.com>
2023-12-04 09:55:51 +0000

    day 2

--- day2/rust/src/main.rs
+++ day2/rust/src/main.rs
@@ -19,21 +19,31 @@ fn parse_input(input: String) -> Vec<Vec<Vec<Vec<String>>>> {
         .collect::<Vec<_>>()
 }
 
-fn part_one(games: Vec<Vec<Vec<Vec<String>>>>) -> u32 {
+fn process(games: Vec<Vec<Vec<Vec<String>>>>) -> (u32, u32) {
     let max_cubes: HashMap<&str, u32> = HashMap::from([("red", 12), ("green", 13), ("blue", 14)]);
-    let mut counter: usize = 0;
+    let mut part_one: usize = 0;
+    let mut part_two: u32 = 0;
 
     for (i, game) in games.into_iter().enumerate() {
         let mut game_possible = true;
+        let mut max_colours: HashMap<&str, u32> =
+            HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
 
-        for rounds in game {
+        for rounds in &game {
             let mut round_possible: Vec<bool> = vec![];
 
             for round in rounds {
                 let count = round[0].parse::<u32>().unwrap();
-                let colour = &round[1];
-                if &count > max_cubes.get(colour.as_str()).unwrap() {
+                let colour = round[1].as_str();
+
+                let cur_max = max_colours.get(colour).unwrap();
+
+                if &count > max_cubes.get(colour).unwrap() {
                     round_possible.push(false)
+                }
+
+                if count > *cur_max {
+                    max_colours.insert(colour, count);
                 }
             }
             if !round_possible.is_empty() {
@@ -41,40 +51,17 @@ fn part_one(games: Vec<Vec<Vec<Vec<String>>>>) -> u32 {
             }
         }
         if game_possible {
-            counter += i + 1;
+            part_one += i + 1;
         }
+        part_two += max_colours.values().product::<u32>();
     }
-    counter as u32
-}
-
-fn part_two(games: Vec<Vec<Vec<Vec<String>>>>) -> u32 {
-    let mut powers: u32 = 0;
-
-    for game in games {
-        let mut max_colours: HashMap<&str, u32> =
-            HashMap::from([("red", 0), ("green", 0), ("blue", 0)]);
-
-        for rounds in &game {
-            for round in rounds {
-                let count = round[0].parse::<u32>().unwrap();
-                let colour = round[1].as_str();
-                let cur_max = max_colours.get(colour).unwrap();
-
-                if count > *cur_max {
-                    max_colours.insert(colour, count);
-                }
-            }
-        }
-        powers += max_colours.values().product::<u32>();
-    }
-    powers
+    (part_one as u32, part_two)
 }
 
 fn main() {
     let input = std::fs::read_to_string("day2.txt").unwrap();
     let games = parse_input(input);
-    let part_one = part_one(games.clone());
-    let part_two = part_two(games);
+    let (part_one, part_two) = process(games);
 
     println!("part 1: {}, part 2: {}", part_one, part_two)
 }
